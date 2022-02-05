6a3d6b800d58c46f1fea9eae7f52d7f6fdb51b60
Carmen Seni Hernandez R <icarhdez@Carmens-MacBook-Air.local>
2022-02-05 12:17:42 -0600

    logic complete shopping cart

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,21 @@ let rockQty= 1
 
 let addBtn=document.querySelector('#quantity-up')
 let minusBtn=document.querySelector('#quantity-down')
+let qtyDisplay= document.querySelector('.total-quantity')
 // add a click event to each button
+//the quantity updates when the plus or minus buttons are cliked
+
 addBtn.addEventListener('click', function(e) {
     rockQty++
-    console.log(rockQty)
+    // the plus button adds 1 to the product quantity
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
 })
 
 minusBtn.addEventListener('click',function(e) {
+    //the quantity cannot drop below 0
+    if(rockQty=1){
     rockQty--
-    console.log(rockQty)
+    //the minus button subtracts 1 to the product quantity
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
+    }
 })
-
-
-// the plus button adds 1 to the product quantity
-
-
-
-
-
-//the minus button subtracts 1 to the product quantity
-
-//the quantity updates when the plus or minus buttons are cliked
-
-//the quantity cannot drop below 0
